9dd2d9f974242e7eaf021cbbeef3e0e1e23201fe
MohammedError <mohammed.secure@outlook.sa>
2025-12-14 13:25:17 +0000

    Fix Betty style errors for bst_remove

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -1,19 +1,63 @@
 #include "binary_trees.h"
 
 /**
+ * successor - Get the next successor (min node in right subtree)
+ * @node: Tree node
+ * Return: pointer to the successor
+ */
+bst_t *successor(bst_t *node)
+{
+	bst_t *ptr = node;
+
+	while (ptr->left)
+		ptr = ptr->left;
+	return (ptr);
+}
+
+/**
+ * replace_two_children - Handles removal of a node with two children
+ * @root: Pointer to the root
+ * @node: Pointer to the node to delete
+ * @succ: Pointer to the successor
+ * Return: New root
+ */
+bst_t *replace_two_children(bst_t *root, bst_t *node, bst_t *succ)
+{
+	if (succ->parent != node)
+	{
+		succ->parent->left = succ->right;
+		if (succ->right)
+			succ->right->parent = succ->parent;
+		succ->right = node->right;
+		node->right->parent = succ;
+	}
+	succ->left = node->left;
+	node->left->parent = succ;
+	succ->parent = node->parent;
+	if (node->parent)
+	{
+		if (node->parent->left == node)
+			node->parent->left = succ;
+		else
+			node->parent->right = succ;
+	}
+	else
+		root = succ;
+	free(node);
+	return (root);
+}
+
+/**
  * bst_remove - removes a node from a Binary Search Tree
  * @root: pointer to the root node of the tree where you will remove a node
  * @value: value to remove in the tree
  *
- * Return: pointer to the new root node of the tree after removing the desired value
+ * Return: pointer to the new root node of the tree
  */
 bst_t *bst_remove(bst_t *root, int value)
 {
-	bst_t *node = root, *parent, *successor;
+	bst_t *node = root, *parent, *succ;
 
-	if (!root)
-		return (NULL);
-	/* Find node */
 	while (node)
 	{
 		if (node->n == value)
@@ -22,47 +66,19 @@ bst_t *bst_remove(bst_t *root, int value)
 	}
 	if (!node)
 		return (root);
-	
 	parent = node->parent;
-	
-	/* Case: 0 or 1 child */
 	if (!node->left || !node->right)
 	{
-		successor = (node->left) ? node->left : node->right;
+		succ = (node->left) ? node->left : node->right;
 		if (parent)
-			(parent->left == node) ? (parent->left = successor) : (parent->right = successor);
+			(parent->left == node) ? (parent->left = succ) : (parent->right = succ);
 		else
-			root = successor;
-		if (successor)
-			successor->parent = parent;
+			root = succ;
+		if (succ)
+			succ->parent = parent;
 		free(node);
 		return (root);
 	}
-	
-	/* Case: 2 children - Find in-order successor */
-	successor = node->right;
-	while (successor->left)
-		successor = successor->left;
-	
-	/* Update successor links if not direct child */
-	if (successor->parent != node)
-	{
-		successor->parent->left = successor->right;
-		if (successor->right)
-			successor->right->parent = successor->parent;
-		successor->right = node->right;
-		node->right->parent = successor;
-	}
-	
-	successor->left = node->left;
-	node->left->parent = successor;
-	successor->parent = node->parent;
-	
-	if (parent)
-		(parent->left == node) ? (parent->left = successor) : (parent->right = successor);
-	else
-		root = successor;
-	
-	free(node);
-	return (root);
+	succ = successor(node->right);
+	return (replace_two_children(root, node, succ));
 }
