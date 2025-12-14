15d824c49789b3e8c18f113004697f03b575576a
MohammedError <mohammed.secure@outlook.sa>
2025-12-14 13:07:08 +0000

    Fix Betty style for Task 25

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -1,37 +1,30 @@
 #include "binary_trees.h"
 
-/**
- * bst_insert - inserts a value in a Binary Search Tree
- * @tree: double pointer to the root node of the BST to insert the value
- * @value: value to store in the node to be inserted
- *
- * Return: pointer to the created node, or NULL on failure
- */
 bst_t *bst_insert(bst_t **tree, int value)
 {
 	bst_t *curr, *new_node;
 
-	if (tree == NULL)
+	if (!tree)
 		return (NULL);
 
-	if (*tree == NULL)
+	if (!*tree)
 	{
 		new_node = binary_tree_node(NULL, value);
-		if (new_node == NULL)
+		if (!new_node)
 			return (NULL);
 		*tree = new_node;
 		return (new_node);
 	}
 
 	curr = *tree;
-	while (curr != NULL)
+	while (curr)
 	{
 		if (value == curr->n)
 			return (NULL);
 
 		if (value < curr->n)
 		{
-			if (curr->left == NULL)
+			if (!curr->left)
 			{
 				new_node = binary_tree_node(curr, value);
 				curr->left = new_node;
@@ -41,7 +34,7 @@ bst_t *bst_insert(bst_t **tree, int value)
 		}
 		else
 		{
-			if (curr->right == NULL)
+			if (!curr->right)
 			{
 				new_node = binary_tree_node(curr, value);
 				curr->right = new_node;
