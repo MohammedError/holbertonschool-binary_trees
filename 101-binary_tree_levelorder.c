ac4740f62859dc40eee056636445f3ef012b9295
MohammedError <mohammed.secure@outlook.sa>
2025-12-14 12:57:55 +0000

    Fix Betty line length for Task 20

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -42,7 +42,7 @@ void print_level(const binary_tree_t *tree, size_t level, void (*func)(int))
 }
 
 /**
- * binary_tree_levelorder - goes through a binary tree using level-order traversal
+ * binary_tree_levelorder - Level-order traversal of a binary tree
  * @tree: pointer to the root node of the tree to traverse
  * @func: pointer to a function to call for each node
  */
